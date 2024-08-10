e656848a667bfd4a046d731919a13b523fe3b4b3
Gary Lynch <lynch2812@googlemail.comgit config --global user.name gary>
2024-08-10 14:04:41 +0100

    Data types

--- Variables.py
+++ Variables.py
@@ -2,3 +2,14 @@ num1=10
 name="Gary"
 #prints out what type of variable it is 
 print(type(name))
+
+#String Variables can be used in single or double quotes  
+apellado ="Lynch"
+print(apellado)
+#Multiple word variable names
+# Camel case example 
+myFathersName="Graham"
+#Pascal case 
+MyFathersName =  "John"
+#Snake case 
+my_father_name = "Stephen"
---
+++ datatypes.py
@@ -0,0 +1,5 @@
+#string data type 
+#most common str,int,float
+x = 3+5j
+print(type(x))
+print(x)
