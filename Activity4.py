8a236fe560b068116331c6e4b858cd85b15d4d58
Gary Lynch <lynch2812@googlemail.comgit config --global user.name gary>
2024-08-12 23:32:57 +0100

    Activity4

---
+++ Activity4.py
@@ -0,0 +1,9 @@
+import random
+#prints a number between 1 and 100
+random_number = random.randrange(1, 100)
+#uses the string and prints the type
+print("Random Number", random_number)
+#converting the number from Int to float 
+print(type(random_number))
+randomNumber =float(random_number)
+print(type(randomNumber))
