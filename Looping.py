cfab61522561c7ed3d36e02f859fb08b5f561481
Gary Lynch <lynch2812@googlemail.comgit config --global user.name gary>
2024-08-13 23:19:16 +0100

    Length

---
+++ Looping.py
@@ -0,0 +1,5 @@
+x = 'i love football'
+#prints all the letters in the variable, you can also put for x in "what word you want" and it will do the same
+for x in x:
+    print(x)
+ 
--- Strings.py
+++ Strings.py
@@ -4,3 +4,9 @@ a major line,
 good practice'''
 
 print(name)
+#square brackets with a number index print the letter starting from 0
+name= 'Gary Lynch'
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
