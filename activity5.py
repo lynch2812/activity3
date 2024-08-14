5574b5cc08a3b2e5e33cf800010f00cce3dff652
Gary Lynch <lynch2812@googlemail.comgit config --global user.name gary>
2024-08-14 22:19:33 +0100

    activity5

---
+++ activity5.py
@@ -0,0 +1,12 @@
+madrid = "This is the Spanish capital"
+london = "This is the English captial"
+paris = "This is the French capital"
+berlin = "This is the German capital"
+rome = "This is the Italian capiltal"
+x = slice(20)
+print(madrid.upper())
+print(london.lower())
+print(paris[x])
+print(len(berlin))
+for rome in rome:
+    print(rome)
