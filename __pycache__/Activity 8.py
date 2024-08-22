1a3aa89a0065e436b0fe147c9417e274f13dae2e
Gary Lynch <lynch2812@googlemail.comgit config --global user.name gary>
2024-08-22 23:13:36 +0100

    bitwise

---
+++ __pycache__/Activity 8.py
@@ -0,0 +1,14 @@
+#Create a list of test scores for a student.
+#Use floor division to calculate the average score.
+#Use comparison operators to determine the grade based on the average score.
+#Use assignment operators to update the student's grade.
+#Use membership operators to check if a specific score exists in the list of scores.
+#Use identity operators to compare objects.
+#Use bitwise operators to perform bitwise operations on the scores.
+
+testScores = [21, 25, 95, 88, 51]
+results = sum(testScores)
+averageResults = len(testScores)
+averageAnswer = results // averageResults
+print(averageAnswer)
+
---
+++ __pycache__/bitwise.py
@@ -0,0 +1,25 @@
+#& The & operator performs a bitwise and operation on coresponding bits of two  integars, ot returns a new integar with bits set to 1  if both inputs are 1 
+
+a = 2
+b = 9 
+result=a&b
+
+print(result)
+ 
+ # OR
+ # | = or turns binary back in to decimal 
+a = 5 
+b = 3
+result = a|b
+print(result)
+
+# << left shift , shifts the bits of an integar left by the number of positions and multiplys the integar
+a = 10
+result = a<<2
+print(result)
+
+# >> right shift divides the integar by the number requested 
+
+a = 20
+result = a>>4
+print(result)
