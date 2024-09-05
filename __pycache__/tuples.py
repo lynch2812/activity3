3d4aaffd564469d13004ac5713b7a4bea4fa2b98
Gary Lynch <lynch2812@googlemail.comgit config --global user.name gary>
2024-09-05 23:04:48 +0100

    tuples

--- __pycache__/tuples.py
+++ __pycache__/tuples.py
@@ -16,4 +16,12 @@ names = ("Gary","Liam","Chris")
 
 for i in names:
  print(i)
- 
+
+ ## tuple methods 
+numbers = 2,8,93,9,8,4,5,2,83,2,5,2,52,2,6
+print(numbers.count(8))
+# when do we use tuples, they are used when the items wont change during the programmes execution i.e coordinates 
+def coordinates():
+ return ("j",4)
+x,y = coordinates()
+print(x,y)
