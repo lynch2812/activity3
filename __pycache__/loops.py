732a53a9771937a3c418f0045c63cfc54ed9b67a
Gary Lynch <lynch2812@googlemail.comgit config --global user.name gary>
2024-09-25 21:58:35 +0100

    loops

---
+++ __pycache__/loops.py
@@ -0,0 +1,18 @@
+#prints out the teams 
+teams = ["man Utd", "Chelsea", "Man City"]
+for team in teams:
+    print(team)
+
+for team in range(1,6):
+    print(team)
+
+
+number = 0 
+while number <5:
+    print(number)
+    number += 1
+
+for i in range(5):
+    if i == 5:
+        break
+    print(i)
