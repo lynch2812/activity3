f2c1e99e98567a6850f60d20f9da428a117cf1e3
Gary Lynch <lynch2812@googlemail.comgit config --global user.name gary>
2024-09-25 21:36:36 +0100

    condition

---
+++ __pycache__/condition.py
@@ -0,0 +1,30 @@
+x = 9
+y = 55 
+if x>10:
+    print("x is greater than 10")
+
+#else:
+  #  print("x is less than")
+
+elif x<10:
+ print(" rubbish ") 
+
+else:
+   print("your learning")
+
+
+a=45
+b=35
+
+if a>35:
+    if b>45:
+         print("both are true")
+    else:
+     print("only one is true")
+
+else:
+ print("neither")
+
+
+
+   
