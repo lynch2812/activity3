50c77045ebee2314b52d1e998fba774ad7e30ad9
Gary Lynch <lynch2812@googlemail.comgit config --global user.name gary>
2024-11-16 12:12:01 +0000

    arrays

--- __pycache__/Arrays.py
+++ __pycache__/Arrays.py
@@ -4,3 +4,15 @@ list1 = [1,2,3,4,5,6]
 from array import array
 # the i tells the code it is using integers 
 myarray= array('i', [1,2,3,4,5,6])
+
+# NumPy (Numerical Python) is a powerful library in Python that provides support for large, 
+# multi-dimensional arrays and matrices, along with a wide collection of 
+# mathematical functions to operate on these arrays efficiently. 
+# It is widely used in scientific computing, data analysis, 
+# machine learning, and other fields requiring numerical computations.
+
+# using [1:4] for example would slice and output numbers 1 - 3
+import numpy as np
+
+myarray = np.array([1,2,3,4,5]) 
+
